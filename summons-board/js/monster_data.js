c1332c739b9d93774593f4917ab2bb7c4d8548c5
shesheshe <shesheshe98@hotmail.com>
2014-05-13 00:51:57 +0800

    use css sprites part 2

--- summons-board/js/monster_data.js
+++ summons-board/js/monster_data.js
@@ -1,7 +1,6 @@
 var data = [
-	{'img':'t.png', 'name':'hero1', 'kind':'1', 'ability1':'t.png', 'ability2':'t.png'},
-	{'img':'t.png', 'name':'hero2', 'kind':'2', 'ability1':'t.png', 'ability2':'t.png'},
-	{'img':'t.png', 'name':'hero3', 'kind':'3', 'ability1':'t.png', 'ability2':'t.png'},
-	{'img':'t.png', 'name':'hero4', 'kind':'1', 'ability1':'t.png', 'ability2':'t.png'},
-	{'img':'t.png', 'name':'hero5', 'kind':'2', 'ability1':'t.png', 'ability2':'t.png'}
+	{'img':'t.png', 'name':'クレン', 'kind':'1', 'ability1':'--', 'ability2':'--'},
+	{'img':'margin-left: -102px', 'name':'クレイソン', 'kind':'1', 'ability1':'--', 'ability2':'--'},
+	{'img':'margin-left: -203px', 'name':'クレメイソン', 'kind':'1', 'ability1':'--', 'ability2':'--'},
+	{'img':'margin-top: -777px', 'name':'獄炎獣クレメイソン', 'kind':'1', 'ability1':'全体化', 'ability2':'--'},
 ];
