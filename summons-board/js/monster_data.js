a73da1135d54c82bb3c7995292c3a75481075c9e
hero.hsu <shesheshe98@hotmail.com>
2014-05-12 15:02:34 +0800

    整理目錄, 修改檔案名稱

---
+++ summons-board/js/monster_data.js
@@ -0,0 +1,7 @@
+var data = [
+	{'img':'t.png', 'name':'hero1', 'kind':'1', 'ability1':'t.png', 'ability2':'t.png'},
+	{'img':'t.png', 'name':'hero2', 'kind':'2', 'ability1':'t.png', 'ability2':'t.png'},
+	{'img':'t.png', 'name':'hero3', 'kind':'3', 'ability1':'t.png', 'ability2':'t.png'},
+	{'img':'t.png', 'name':'hero4', 'kind':'1', 'ability1':'t.png', 'ability2':'t.png'},
+	{'img':'t.png', 'name':'hero5', 'kind':'2', 'ability1':'t.png', 'ability2':'t.png'}
+];
