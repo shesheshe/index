cd3d1fb4cf33de25d9b9ee8e2e0214d2268edfc6
unknown <ben891095@gmail.com>
2014-06-13 01:56:38 +0800

    2014/06/13 01:56

--- games/tower_def/map_build.js
+++ games/tower_def/map_build.js
@@ -1,12 +1,56 @@
-//解析地圖並且畫出來
-draw_back_ground("map_01")
+//解析地圖並且畫出來 x=i , y=j
+draw_back_ground("map_02")
 function draw_back_ground(map_no) {
-    alert(this[map_no]["back_ground"]);
-    
-    for(i = 0; i < play_unit_x; i++ ) {       //x
-        for(j = 0; j < play_unit_y; j++ ) {   //y
-
-            back_ground.drawImage(image_back_ground, 0*unit, 0*unit, 16, 16, i*unit, j*unit, 32, 32);
+    //背景圖片開始位置
+	var strat_x = this[map_no]["back_ground"]*2;
+    var strat_y = 0;	
+	//背景
+    for(var i = 0; i < play_unit_x; i++ ) {       //x
+        for(var j = 0; j < play_unit_y; j++ ) {   //y	
+            back_ground.drawImage(image_back_ground, strat_x*unit, strat_y*unit, unit, unit, i*unit, j*unit, unit, unit);
+			//back_ground.fillText(j, i*unit, j*unit);
         }
     }
+	
+	var road_unit_h = this[map_no]["road_unit_h"];
+    var route = this[map_no]["route"];	
+	//道路
+	for (var key in route) {
+		if (key != 0) {
+			//x軸
+			//左到右
+			if ((route[key][0]-route[key-1][0]) > 0) {
+				for (var i = route[key-1][0]; i <= route[key][0]; i++) {
+					for (var h = 0; h < road_unit_h; h++) {
+						back_ground.drawImage(image_back_ground, strat_x*unit, (strat_y+2)*unit, unit, unit, i*unit, (route[key][1]+h)*unit, unit, unit);
+					}
+				}
+			}
+			//右到左
+			if ((route[key][0]-route[key-1][0]) < 0) {
+				for (var i = route[key-1][0]; i >= route[key][0]; i--) {
+					for (var h = 0; h < road_unit_h; h++) {
+						back_ground.drawImage(image_back_ground, strat_x*unit, (strat_y+2)*unit, unit, unit, i*unit, (route[key][1]+h)*unit, unit, unit);
+					}
+				}
+			}
+			//y軸
+			//上到下
+			if ((route[key][1]-route[key-1][1]) > 0) {
+				for (var j = route[key-1][1]; j < route[key][1]+road_unit_h; j++) {
+					for (var h = 0; h < road_unit_h; h++) {
+						back_ground.drawImage(image_back_ground, strat_x*unit, (strat_y+2)*unit, unit, unit, (route[key][0]+h)*unit, j*unit, unit, unit);
+					}
+				}
+			}
+			//下到上
+			if ((route[key][1]-route[key-1][1]) < 0) {
+				for (var j = route[key-1][1]+road_unit_h-1; j > route[key][1]; j--) {
+					for (var h = 0; h < road_unit_h; h++) {
+						back_ground.drawImage(image_back_ground, strat_x*unit, (strat_y+2)*unit, unit, unit, (route[key][0]+h)*unit, j*unit, unit, unit);
+					}
+				}
+			}
+		}
+	}
 }
--- games/tower_def/map_data.js
+++ games/tower_def/map_data.js
@@ -1,12 +1,21 @@
 //橫列 直欄
 var map_01 = {
-    //1 => 第1欄 image_back_ground
+    //0 => 第1欄 image_back_ground
     "back_ground" : 0,
     //路寬(高)
     "road_unit_h" : 3,
     //back_ground_unit
     "route" : [
-        [0,5],[54,5],[54,15],[10,15]
+        [0,5],[54,5],[54,15],[10,15],[10,25],[63,25]
+    ],
+    "enemy" : []
+}
+
+var map_02 = {
+    "back_ground" : 1,
+    "road_unit_h" : 3,
+    "route" : [
+        [0,5],[15,5],[15,25],[45,25],[45,5],[63,5]
     ],
     "enemy" : []
 }
